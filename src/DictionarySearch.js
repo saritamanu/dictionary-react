0d04ef69bf8b5c788c453379c1ef803bf8b3ead4
saritamanu <sara.m.dias22@gmail.com>
2024-02-18 18:36:19 +0000

    added axios and API dictionary

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function DictionarySearch() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings);
+  }
   function searchTheWord(event) {
     event.preventDefault();
+    const apiKey = "7a1ob024b4t960f923f458712f6094e1";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     alert(`You are searching for ${keyword}`);
+    axios.get(apiUrl).then(handleResponse);
+    console.log(apiUrl);
   }
 
   function theWord(event) {
